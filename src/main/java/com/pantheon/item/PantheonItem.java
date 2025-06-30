917637de76818d650899170c714f820ec49dc776
OliverKeefe <oliverkeefe@tutanota.com>
2025-06-30 20:38:59 +0100

    feat: Add PantheonItem

---
+++ src/main/java/com/pantheon/item/PantheonItem.java
@@ -0,0 +1,30 @@
+package com.pantheon.item;
+
+import com.pantheon.Pantheon;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class PantheonItem {
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Pantheon.MOD_ID, name));
+
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static void initialize() {
+
+    }
+
+
+
+}
